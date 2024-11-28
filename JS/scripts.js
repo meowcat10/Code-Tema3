6a92fb6b0359d06f4d07c27a365bde4046b61471
MeowCat <Andreas@mitkas.com>
2024-11-28 14:17:49 +0100

    Finished - Version 1

--- JS/scripts.js
+++
@@ -1,9 +0,0 @@
-
-// Creates an object with information about the shoes.
-let redbullShoes = {
-    name: 'Redbull X Crocs - Limited Edition',
-    price: 1000
-};
-
-
-document.querySelector('.RedBullProduct').innerText = `${redbullShoes.name}\n$${redbullShoes.price}`;
